38e40f314aeda3dcddc1f2a07b030105366a4283
Alessandro Portes <alessandro.portes@gmail.com>
2021-04-22 08:46:11 -0300

    aula 2 final

--- podcastrnext/src/pages/index.tsx
+++ podcastrnext/src/pages/index.tsx
@@ -1,3 +1,43 @@
-export default function Home() {
+// SPA - Single Page Aplication
+// SSR - Server Side Rendering
+// SSG - Static Site Generation
+
+// import { useEffect } from 'react';
+
+export default function Home(props) {
+  // SPA - Essa chamada não é indexada pelo GOOGLE se desativar o javascript do browser para de funcionar essa estrategia
+  // useEffect(() => {
+  //   fetch('http://localhost:3333/episodes')
+  //     .then((response) => response.json())
+  //     .then((data) => console.log(data));
+  // }, []);
+  // Fim SPA
+
+  //SSR
+  // console.log(props.episodes);
+
   return <div>Index</div>;
+}
+
+//SSR
+// export async function getServerSideProps() {
+//   const response = await fetch('http://localhost:3333/episodes');
+//   const data = await response.json();
+//   return {
+//     props: {
+//       episodes: data,
+//     },
+//   };
+// }
+
+//SSG
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  };
 }
