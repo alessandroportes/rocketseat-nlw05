bf26a0a26ec3f1fe0652f168198334e3d2c72b3f
Alessandro Portes <alessandro.portes@gmail.com>
2021-04-23 16:41:14 -0300

    Aula 3 - 1h21

---
+++ podcastrnext/src/pages/episodes/[slug].tsx
@@ -0,0 +1,81 @@
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import Image from 'next/image';
+
+import { api } from '../../services/api';
+import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
+
+import styles from './episode.module.scss';
+
+type Episode = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  members: string;
+  duration: number;
+  durationAsString: string;
+  description: string;
+  url: string;
+  publishedAt: string;
+};
+
+type EpisodeProps = {
+  episode: Episode[];
+};
+
+export default function Episode({ episode }) {
+  return (
+    <div className={styles.episode}>
+      <div className={styles.thumbnailContainer}>
+        <button type="button">
+          <img src="/arrow-left.svg" alt="Voltar" />
+        </button>
+        <Image
+          width={700}
+          height={160}
+          src={episode.thumbnail}
+          objectFit="cover"
+        />
+        <button type="button">
+          <img src="/play.svg" alt="Tocar" />
+        </button>
+
+        <header>PAREI AQUI Aula 3 1h21</header>
+      </div>
+    </div>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { slug } = ctx.params;
+  const { data } = await api.get(`/episodes/${slug}`);
+
+  const episode = {
+    id: data.id,
+    title: data.title,
+    thumbnail: data.thumbnail,
+    members: data.members,
+    publishedAt: format(parseISO(data.published_at), 'd MMM yy', {
+      locale: ptBR,
+    }),
+    duration: Number(data.duration),
+    durationAsString: convertDurationToTimeString(Number(data.file.duration)),
+    description: data.description,
+    url: data.file.url,
+  };
+
+  return {
+    props: {
+      episode,
+    },
+    revalidate: 60 * 60 * 24, //24 horas
+  };
+};
